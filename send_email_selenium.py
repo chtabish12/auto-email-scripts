558bccca59fd534c038349a86b671e32b3109500
ch tabish <72700036+chtabish12@users.noreply.github.com>
2023-05-23 17:24:43 +0500

    selenium script added

--- send_email_selenium.py
+++ send_email_selenium.py
@@ -20,8 +20,6 @@ def send_email(username, password, from_name, recipient, subject, message):
         # Submit the login form
         driver.find_element(By.ID, "form_Valider").click()
         time.sleep(30)  
-        # Wait for the inbox page to load
-        WebDriverWait(driver, 5).until(EC.title_contains("Inbox"))
 
         # Navigate to the compose email page
         driver.get("https://messagerie.ac-aix-marseille.fr/roundcube/?_task=mail&_action=compose&_id=")
@@ -30,18 +28,16 @@ def send_email(username, password, from_name, recipient, subject, message):
         driver.find_element(By.CSS_SELECTOR, 'li.input input[type="text"]').send_keys(recipient)
         driver.find_element(By.ID, "compose-subject").send_keys(subject)
 
-        # Switch to the message compose frame
-        frame = driver.find_element(By.NAME, "messagecompose")
-        driver.switch_to.frame(frame)
+        element = driver.find_element(By.TAG_NAME, "html")
+        element.get_attribute("innerText")
+        element.send_keys(message)
 
-        # Enter the message content
-        driver.find_element(By.ID, "tinymce").send_keys(message)
 
         # Switch back to the default content
         driver.switch_to.default_content()
 
         # Click the send button
-        driver.find_element(By.XPATH, "//button[text()='Send']").click()
+        driver.find_element(By.ID, "rcmbtn110").click()
 
         # Wait for the email to be sent
         time.sleep(5)  # You can adjust the delay as needed
